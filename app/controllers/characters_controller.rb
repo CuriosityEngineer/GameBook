32a1c62694bd4715d5e6e958d203e85acae87550
CuriosityEngineer <aeshta@gmail.com>
2016-07-03 13:25:22 +0200

    Add NEW chracater page and made notes to what should be on it

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,4 +1,11 @@
 class CharactersController < ApplicationController
+  # this is to check if user has permision to go to the  pages
+  before_action :authenticate_user!, except: [:index, :show]
+
   def index
   end
+
+  def new
+  end
+  
 end
