eb2d92e36aeea4385d90c699ddc81be5c916e474
CuriosityEngineer <aeshta@gmail.com>
2016-07-03 16:13:15 +0200

    Add create character function

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,11 +1,27 @@
 class CharactersController < ApplicationController
   # this is to check if user has permision to go to the  pages
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :new]
 
   def index
   end
 
   def new
   end
-  
+
+  def create
+    @character = Character.new(character_params)
+    @character.save
+
+    redirect_to @character
+  end
+
+  def show
+    @character = Character.find(params[:id])
+  end
+
+  private
+    def character_params
+      params.require(:characters).permit(:name)
+    end
+
 end
